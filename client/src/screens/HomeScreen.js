3cf43b9a5aa4aca856024e3159fecf67c07335dd
SAMADIPERERA <samadiperera2011@gmail.com>
2024-03-14 09:56:10 +0530

    Add server

---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function HomeScreen() {
+ 
+    return (
+        <div>
+            <h1>Home Screen</h1>
+        </div>
+    );
+}
+
+export default HomeScreen;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require("express");
+
+const app = express();
+
+/*const dbConfig = require('./db')
+const hallRouter = require('./routes/hallRouter')
+
+app.use('/api/halls', hallRouter)*/
+
+const port= process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log('Node Server Started using nodemon'));
